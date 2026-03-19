51d65fad24b1accd3ea770061daac24892bac5a8
nzhumabay24-lab <n.zhumabay24@apec.edu.kz>
2026-03-19 23:36:21 +0500

    Add files via upload

---
+++ task3/task3.cs
@@ -0,0 +1,10 @@
+﻿int[] numbers = { -4, -3, -2, -1, 0, 1, 2, 3, 4 };
+int result = 0;
+foreach (int number in numbers)
+{
+    if (number > 0)
+    {
+        result++;
+    }
+}
+Console.WriteLine($"Число элементов больше нуля: {result}");
---
+++ task3/task3_2.cs
@@ -0,0 +1,15 @@
+﻿int[] numbers = { -4, -3, -2, -1, 0, 1, 2, 3, 4 };
+
+int n = numbers.Length; // длина массива
+int k = n / 2;          // середина массива
+int temp;               // вспомогательный элемент для обмена значениями
+for (int i = 0; i < k; i++)
+{
+    temp = numbers[i];
+    numbers[i] = numbers[n - i - 1];
+    numbers[n - i - 1] = temp;
+}
+foreach (int i in numbers)
+{
+    Console.Write($"{i} \t");
+}
